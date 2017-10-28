24e4d2b150bd02fd0cfeb1fc2b87f3dce6b48905
Arjan van Wijk <thanarie@gmail.com>
2017-10-28 21:31:47 +0200

    Map default import-names

--- .tslintrc.js
+++ .tslintrc.js
@@ -11,6 +11,14 @@ module.exports = {
     'variable-name': [ true, 'ban-keywords', 'check-format', 'allow-pascal-case', 'allow-leading-underscore'],
     'max-line-length': [true, 100],
     'no-increment-decrement': false,
-    'strict-boolean-expressions': false
+    'strict-boolean-expressions': false,
+    'import-name': [ true, {
+      'jquery': '$',
+      'underscore': '_',
+      'lodash': '_',
+      'knockout': 'ko',
+      'gsap': 'TweenMax'
+    }],
+
   },
 };
