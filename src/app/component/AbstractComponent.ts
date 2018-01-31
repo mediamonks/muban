a971f7372bae6b0062b1f27144018720eabb92ba
Jesse Linthorst <jesse@mediamonks.com>
2018-01-31 11:37:58 +0100

    Convert component root element data attributes to a JavaScript object

--- src/app/component/AbstractComponent.ts
+++ src/app/component/AbstractComponent.ts
@@ -1,7 +1,23 @@
+import camelCase from 'lodash/camelCase';
+
 export default class AbstractComponent {
-  constructor(public element: HTMLElement) {}
+  protected data: { [key: string]: string };
+
+  constructor(public element: HTMLElement) {
+    this.data = Object.values(element.attributes)
+      .map(attribute => ({ name: attribute.nodeName, value: attribute.value }))
+      .filter(node => node.name.includes('data-'))
+      .reduce(
+        (accumulator, node) => ({
+          ...accumulator,
+          [camelCase(node.name.replace(/^data-/, ''))]: node.value,
+        }),
+        {},
+      );
+  }
 
   dispose() {
     this.element = null;
+    this.data = null;
   }
 }
