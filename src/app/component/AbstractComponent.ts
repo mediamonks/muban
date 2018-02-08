e5ac931348568c44c37bfe00b259ec0d1e810d39
Jesse Linthorst <jesse@mediamonks.com>
2018-02-08 16:03:50 +0100

    Remove data attribute processing

--- src/app/component/AbstractComponent.ts
+++ src/app/component/AbstractComponent.ts
@@ -1,19 +1,7 @@
-import camelCase from 'lodash/camelCase';
-
 export default class AbstractComponent {
   protected data: { [key: string]: string };
 
-  constructor(public element: HTMLElement) {
-    this.data = Object.values(element.attributes)
-      .filter(attribute => attribute.nodeName.startsWith('data-'))
-      .reduce(
-        (accumulator, attribute) => ({
-          ...accumulator,
-          [camelCase(attribute.nodeName.replace(/^data-/, ''))]: attribute.value,
-        }),
-        {},
-      );
-  }
+  constructor(public element: HTMLElement) {}
 
   /**
    * @public
