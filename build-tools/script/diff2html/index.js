5caca56ddefc75c4442a66e4a0e514c7eaa448c1
Serhii Teplov <serhii.teplov@mediamonks.com>
2018-04-10 13:13:01 +0200

    update report template. show commit information in header

--- build-tools/script/diff2html/index.js
+++ build-tools/script/diff2html/index.js
@@ -1,6 +1,9 @@
 const { exec } = require('child_process');
+const path = require('path');
 const { mkdirsSync } = require('fs-extra');
 const inquirer = require('inquirer');
+const addRootDir = require('app-root-dir');
+const Diff2HtmlReportBuilder = require('./diff2html-report-builder');
 
 const OUTPUT_DIR = 'dist/diff';
 const REPORT_NAME = 'templates.html';
@@ -58,15 +61,20 @@ inquirer.prompt(questions).then(answers => {
   const from = answers.from === 'value' ? answers.fromValue : 'origin/master';
   const to = answers.from === 'value' ? answers.toValue : '';
 
-  const command = `diff2html -s side -f html -F ${OUTPUT_DIR}/${REPORT_NAME} -- -M ${from} ${to} -- ${EXT}`;
-
   mkdirsSync(OUTPUT_DIR);
 
-  exec(command, err => {
-    if (err) {
-      console.error(`Diff2html error occurred: ${err}`);
-
-      return false;
-    }
+  const diff2Html = new Diff2HtmlReportBuilder({
+    outputFormat: 'side-by-side',
+    showFiles: true,
   });
+
+  diff2Html.build(
+    {
+      from,
+      to,
+      ext: EXT
+    },
+    // `-M ${from} ${to} -- ${EXT}`,
+    path.resolve(addRootDir.get(), OUTPUT_DIR, REPORT_NAME),
+  );
 });
