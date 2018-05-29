1cfee7feecd2967d6eca0f90009aa04e30c98201
Arjan van Wijk <thanarie@gmail.com>
2018-05-29 10:06:46 +0200

    Update muban-core and muban-convert-hbs

--- .tslintrc.js
+++ .tslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
     'prefer-array-literal': [ true, { 'allow-type-parameters': true } ],
     'variable-name': [ true, 'ban-keywords', 'check-format', 'allow-pascal-case', 'allow-leading-underscore'],
-    'max-line-length': [true, 100],
+    'max-line-length': false,
     'no-increment-decrement': false,
     'strict-boolean-expressions': false,
     'import-name': false,
