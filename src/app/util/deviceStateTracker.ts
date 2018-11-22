52178e4a2d1cdbab1d0890d8dedeb91bda3825f8
Arjan van Wijk <thanarie@gmail.com>
2018-11-22 12:34:29 +0100

    Rename folder to `shared-variable`

--- src/app/util/deviceStateTracker.ts
+++ src/app/util/deviceStateTracker.ts
@@ -1,7 +1,7 @@
 /* global process */
 
 import DeviceStateTracker from 'seng-device-state-tracker';
-import mq from '../data/variable/media-queries.json';
+import mq from '../data/shared-variable/media-queries.json';
 
 const cleanMediaQueries = Object.keys(mq.mediaQueries).reduce((result, key) => {
   // eslint-disable-next-line no-param-reassign
