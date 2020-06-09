c67d1b80bde43236b26475d2ee6f3a74da9d33c7
Thijs <thijs@mediamonks.com>
2020-06-09 15:57:12 -0300

    use @mediamonks/eslint-config for eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,10 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
-  },
   env: {
     browser: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['import', 'prettier'],
+  extends: ['@mediamonks'],
   settings: {
     // webpack integration
     'import/resolver': {
@@ -18,31 +13,5 @@ module.exports = {
         config: 'build-tools/config/webpack/webpack.conf.base.js',
       },
     },
-  },
-  rules: {
-    'import/extensions': [
-      'error',
-      'always',
-      // hide known extensions that are resolved by webpack
-      {
-        js: 'never',
-        ts: 'never',
-        hbs: 'never',
-      },
-    ],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
-    // prettier compatibility
-    'max-len': ['error', 100],
-    'prettier/prettier': [
-      'error',
-      { singleQuote: true, trailingComma: 'all', printWidth: 100, tabWidth: 2 },
-    ],
-    // only for use with getter-setters
-    'no-underscore-dangle': 0,
-    // to correctly work on windows with some tools that create windows line-endings
-    // this will be correct by git when committed
-    'linebreak-style': 0
   },
 };
