9f6c97f271aa3a6b3703396b5c2eaf3e04ae5e6d
Arjan van Wijk <thanarie@gmail.com>
2020-09-05 12:36:14 +0200

    Add explicit newer versions of autoprefixer and flat

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-/* eslint-disable */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
-    autoprefixer( {browsers: ['last 3 iOS versions', 'last 3 versions', 'ie >= 9']} )
-  ]
+    autoprefixer({ overrideBrowserslist: ['last 3 iOS versions', 'last 3 versions', 'ie >= 9'] }),
+  ],
 };
