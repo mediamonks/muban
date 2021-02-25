f5c0ea5cd86dee44acb6b318812c021c3070531c
Thi.js <thijs+github@mediamonks.com>
2021-02-25 13:44:37 -0300

    change type casting for type generic
    Co-authored-by: Arjan van Wijk <thanarie@gmail.com>

--- src/app/bootstrap.dist.ts
+++ src/app/bootstrap.dist.ts
@@ -32,7 +32,7 @@ context.keys().forEach(key => {
 });
 
 // Makes the website interactive
-const appElement = document.querySelector('#app') as HTMLElement;
+const appElement = document.querySelector<HTMLElement>('#app');
 if (!appElement) {
   throw new ReferenceError('Could not find DOM element with id "app"');
 }
